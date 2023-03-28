c2a93bb56d5b06ad07afbde34de121941b9ac56a
fernandoallisson <fernando.alisson@ufpe.br>
2023-03-28 19:00:06 -0300

    Adicionando arquivos ao repositório

---
+++ fundamentos/secao-3-introducaoJavaScript/dia-1-primeirosPassosJS/desafios JavaScript/04-leituraLivro.js
@@ -0,0 +1,21 @@
+/* Ana começou a ler o livro "O guia do mochileiro das galáxias", que tem 208 páginas. Após 2 minutos ela leu 4 páginas. 
+Crie um algoritmo que calcula e imprime o tempo em minutos que Ana levará para ler todo o livro seguindo o formato:
+Ana levará X minutos para ler o livro todo. */
+
+let pages = 208; 
+let time = (208*2/4)
+
+console.log(`Ana levará ${time} minutos para ler o livro todo`)
+
+
+
+
+
+
+
+
+
+
+
+
+
