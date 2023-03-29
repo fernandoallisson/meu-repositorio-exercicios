ea6bd2c4b013cd39ae69ba03212d2ce334bac012
fernandoallisson <fernando.alisson@ufpe.br>
2023-03-29 13:30:29 -0300

    Realização de exercício 10 da plataforma

---
+++ fundamentos/secao-3-introducaoJavaScript/dia-1-primeirosPassosJS/ex10/script.js
@@ -0,0 +1,9 @@
+/* Utilize if/else para escrever um código que se inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda. A partir dos valores, o código deve calcular o lucro (ou seja, o valor de venda menos o custo do produto) da empresa ao vender mil produtos. */ 
+
+let custoProduto = 10; 
+let vendaProduto = 15; 
+let vendas = 1000; 
+
+let res = (vendaProduto - custoProduto)*vendas
+
+console.log(res)
